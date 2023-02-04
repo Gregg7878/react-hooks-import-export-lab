82a5274aea1e04368ee00ab6739a11d0c5a5e91c
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-02-04 14:59:28 +0300

    modified the about and home codes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        {Lisa} is a Web Developer from {New York}
       </h1>
     </div>
   );
